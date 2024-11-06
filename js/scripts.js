2485d0092b5eb62def6929855992b5b680a6686b
Muhaisin Biruar <mxebiruar@addu.edu.ph>
2024-11-06 16:03:13 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,35 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+function convert() 
+        {
+            let userChoice;
+            let number, converter;
+
+
+            userChoice = document.getElementById("choice").value * 1;
+            number = document.getElementById("num").value * 1;
+
+
+            console.log(userChoice);
+            console.log(number);
+
+
+            if (userChoice == 1)
+            {
+                converter = (number - 32) * (5 / 9); // Fahrenheit to Celsius
+            }
+            else if (userChoice == 2)
+            {
+                converter = (number * 9 / 5) + 32; // Celsius to Fahrenheit
+            }
+            else if (userChoice == 3)
+            {
+                converter = number * 3.281; // Meter to Feet
+            }
+            else
+            {
+                converter = number * 0.3048; // Feet to Meters
+            }
+
+
+            document.getElementById("convertLabel").innerHTML = 'The converted value of ' + number + ' is:';
+            document.getElementById("convertedVal").value = converter.toFixed(3);  
+        }
