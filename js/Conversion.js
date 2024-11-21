af14794f24c31d7fb4718e7db84464e926bf5013
Muhaisin Biruar <mxebiruar@addu.edu.ph>
2024-11-21 19:57:51 +0800

    Update Conversion.js

--- js/Conversion.js
+++ js/Conversion.js
@@ -58,3 +58,13 @@ function convert()
         (() => {
             convert();
         })();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+
+    // Toggle the active class on the menu
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+});
